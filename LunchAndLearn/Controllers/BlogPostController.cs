32d99843ec80fddd441417aca1b89904db2ae6b4
Ty Moodley <ty_moodley@yahoo.com>
2014-04-02 14:29:48 -0400

    First Commit
    First Lunch and Learn on ASP.NET MVC5.

---
+++ LunchAndLearn/Controllers/BlogPostController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Data;
+
+namespace LunchAndLearn.Controllers
+{
+    public class BlogPostController : Controller
+    {
+        private FunWithSignalREntities db = new FunWithSignalREntities();
+
+        // GET: /BlogPost/
+        public ActionResult Index()
+        {
+            return View(db.BlogPosts.ToList());
+        }
+
+        // GET: /BlogPost/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogPost blogpost = db.BlogPosts.Find(id);
+            if (blogpost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogpost);
+        }
+
+        // GET: /BlogPost/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /BlogPost/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include="Id,Title,Post,Active,UpdateBy")] BlogPost blogpost)
+        {
+            if (ModelState.IsValid)
+            {
+                db.BlogPosts.Add(blogpost);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(blogpost);
+        }
+
+        // GET: /BlogPost/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogPost blogpost = db.BlogPosts.Find(id);
+            if (blogpost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogpost);
+        }
+
+        // POST: /BlogPost/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include="Id,Title,Post,Active,UpdateBy")] BlogPost blogpost)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(blogpost).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(blogpost);
+        }
+
+        // GET: /BlogPost/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BlogPost blogpost = db.BlogPosts.Find(id);
+            if (blogpost == null)
+            {
+                return HttpNotFound();
+            }
+            return View(blogpost);
+        }
+
+        // POST: /BlogPost/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            BlogPost blogpost = db.BlogPosts.Find(id);
+            db.BlogPosts.Remove(blogpost);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ LunchAndLearn/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LunchAndLearn.Startup))]
+namespace LunchAndLearn
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
